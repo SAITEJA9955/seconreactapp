73c105d8b339792fae9c0ea25a4edb9d0a9ddf2f
SAITEJA9955 <sivasaitejat1995@gmail.com>
2024-09-20 12:04:19 +0530

    Add files via upload

---
+++ second-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Happy Ganesh Chaturthi</h1>
+      <img src='https://www.theindianwire.com/wp-content/uploads/2024/09/Happy-Ganesh-Chaturthi-1024x724.jpg'></img>
+    </div>
+  );
+}
+
+export default App;
